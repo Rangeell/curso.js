644d63933e5d29f94bf2dcc7f69d586af44e930a
Rangel <breno.r.ramos1@gmail.com>
2026-05-09 08:59:41 -0300

    att ex014

--- class013/ex0014/js/script.js
+++ class013/ex0014/js/script.js
@@ -2,15 +2,15 @@
 
 // VARIABLES - time
 var agora = new Date()
-var hora = agora.getHours()
+var hora = 18
 var minuto = agora.getMinutes()
 
 // VARIABLES - Select
 var body = document.body
 var textoHora = document.querySelector('#horaAtual')
 var saudacao = document.querySelector('#saudacao')
-var bordaImagemNigth = document.querySelectorAll('img')[2]
-var bordaImagemAfternoon = document.querySelectorAll('img')[1]
+var img = document.querySelector('img')
+// var bordaImagemAfternoon = document.querySelectorAll('img')[1]
 
 // VARIABLES - Day Select
 var day = document.querySelector('#day')
@@ -22,54 +22,59 @@ if (hora == 0) {
     textoHora.innerHTML = `Agora é meia-noite (${hora}h:${minuto}min).`
     saudacao.innerHTML = 'Boa madrugada!'
     body.style.backgroundColor = 'var(--corNoite)'
-    day.style.display = 'none'
-    night.style.display = 'block'
-    bordaImagemNigth.style.borderColor = 'var(--corNoite)'
-    bordaImagemNigth.style.outlineColor = 'var(--corNoite)'
+    
+    img.style.borderColor = 'var(--corNoite)'
+    img.style.outlineColor = 'var(--corNoite)'
+    img.src = 'images/night.jpg'
+    img.alt = 'Imagem de um anoitecer em uma cidade moderna'
 } else if (hora == 1) {
     textoHora.innerHTML = `Agora é ${hora}h:${minuto}min.`
     saudacao.innerHTML = 'Boa madrugada!'
     body.style.backgroundColor = 'var(--corNoite)'
-    day.style.display = 'none'
-    night.style.display = 'block'
-    bordaImagemNigth.style.borderColor = 'var(--corNoite)'
-    bordaImagemNigth.style.outlineColor = 'var(--corNoite)'
+    
+    img.style.borderColor = 'var(--corNoite)'
+    img.style.outlineColor = 'var(--corNoite)'
+    img.src = 'images/night.jpg'
+    img.alt = 'Imagem de um anoitecer em uma cidade moderna'
 } else if (hora > 1 && hora < 6) {
     textoHora.innerHTML = `Agora são ${hora}h:${minuto}min.`
     saudacao.innerHTML = 'Boa madrugada!'
     body.style.backgroundColor = 'var(--corNoite)'
-    day.style.display = 'none'
-    night.style.display = 'block'
-    bordaImagemNigth.style.borderColor = 'var(--corNoite)'
-    bordaImagemNigth.style.outlineColor = 'var(--corNoite)'
+    
+    img.style.borderColor = 'var(--corNoite)'
+    img.style.outlineColor = 'var(--corNoite)'
+    img.src = 'images/night.jpg'
+    img.alt = 'Imagem de um anoitecer em uma cidade moderna'
 } else if (hora >= 6 && hora < 12) {
     textoHora.innerHTML = `Agora são ${hora}h:${minuto}min `
     saudacao.innerHTML = 'Bom dia!'
-    body.style.backgroundColor = 'var(--corDia)'
 } else if (hora == 12) {
     textoHora.innerHTML = `Agora é meio dia (${hora}h:${minuto}min).`
     saudacao.innerHTML = 'Boa tarde!'
     body.style.backgroundColor = 'var(--corTarde)'
-    day.style.display = 'none'
-    afternoon.style.display = 'block'
-    bordaImagemAfternoon.style.borderColor = 'var(--corTarde)'
-    bordaImagemAfternoon.style.outlineColor = 'var(--corTarde)'
+
+    img.style.borderColor = 'var(--corTarde)'
+    img.style.outlineColor = 'var(--corTarde)'
+    img.src = 'images/afternoon.jpg'
+    img.alt = 'Imagem de um entardecer'
 } else if (hora > 12 && hora <= 17) {
     textoHora.innerHTML = `Agora são ${hora}h:${minuto}min.`
     saudacao.innerHTML = 'Boa tarde!'
     body.style.backgroundColor = 'var(--corTarde)'
-    day.style.display = 'none'
-    afternoon.style.display = 'block'
-    bordaImagemAfternoon.style.borderColor = 'var(--corTarde)'
-    bordaImagemAfternoon.style.outlineColor = 'var(--corTarde)'
+    
+    img.style.borderColor = 'var(--corTarde)'
+    img.style.outlineColor = 'var(--corTarde)'
+    img.src = 'images/afternoon.jpg'
+    img.alt = 'Imagem de um entardecer'
 } else if (hora > 17) {
     textoHora.innerHTML = `Agora são ${hora}h:${minuto}min.`
     saudacao.innerHTML = 'Boa noite!'
     body.style.backgroundColor = 'var(--corNoite)'
-    day.style.display = 'none'
-    night.style.display = 'block'
-    bordaImagemNigth.style.borderColor = 'var(--corNoite)'
-    bordaImagemNigth.style.outlineColor = 'var(--corNoite)'
+    
+    img.style.borderColor = 'var(--corNoite)'
+    img.style.outlineColor = 'var(--corNoite)'
+    img.src = 'images/night.jpg'
+    img.alt = 'Imagem de um anoitecer em uma cidade moderna'
 }
 
 
