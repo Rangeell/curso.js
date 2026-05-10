67396653cac12f57b9d85f10d1da90b8df48a547
Rangel <breno.r.ramos1@gmail.com>
2026-05-10 11:15:32 -0300

    att ex016

--- class015ex/ex016/js/script.js
+++ class015ex/ex016/js/script.js
@@ -1,4 +1,5 @@
-/* var inicio = 2
+/*
+var inicio = 20
 var fim = 10
 var passo = 2
 
@@ -20,15 +21,37 @@ var txtInicio = document.querySelector('#iinicio')
 var txtFim = document.querySelector('#ifim')
 var txtPasso = document.querySelector('#ipasso')
 
+// Select p#contar
+var info = document.querySelector('#contar')
+info.innerText = "Preparando contagem..."
+
+var contando = document.querySelector('#contando')
+
 // FUNCTION AREA
 
 // Contar
 function Contar() {
-    var inicio = txtInicio.value
-    var fim = txtFim.value
-    var passo = txtPasso.value
+    var inicio = Number(txtInicio.value)
+    var fim = Number(txtFim.value)
+    var passo = Number(txtPasso.value)
 
-    // Test
-    // console.log(`${inicio} ${fim} ${passo}`)
+    if (inicio == 0) {
+        info.innerHTML = 'Impossível contar sem um valor de início! Escolha um número.'
+    } else if (inicio > fim) {
+        alert('O número de início não pode ser maior que número de fim')
+        info.innerHTML = "O número de início não pode ser maior que o número de fim."
+    } else if (passo < 1) {
+        alert('Passo inválido! Considerando passo 1')
+        passo++
+    } else {
+        for (inicio; inicio <= fim; inicio += passo) {
+            contando.innerHTML = 'Contando..'
+            info.innerHTML = `${inicio} ${soma}`
+            
+            console.log(`${soma}`)
+            console.log(`${inicio} ${fim} ${passo}`)
+        }
+    }
 }
 
+// Test
