da4c99137a1a9fcf9cadb1be26383f33c52562c3
Rangel <breno.r.ramos1@gmail.com>
2026-05-11 13:13:49 -0300

    att ex015

--- class015ex/ex016/js/script.js
+++ class015ex/ex016/js/script.js
@@ -4,11 +4,6 @@
 let button = document.querySelector('input[type="button"]')
 button.addEventListener('click', Contar)
 
-// Select text boxes
-let txtInicio = document.querySelector('#iinicio')
-let txtFim = document.querySelector('#ifim')
-let txtPasso = document.querySelector('#ipasso')
-
 // Select p#contar
 let info = document.querySelector('#contar')
 info.innerText = "Preparando contagem..."
@@ -19,29 +14,37 @@ let contando = document.querySelector('#contando')
 
 // Contar
 function Contar() {
-    let inicio = Number(txtInicio.value)
-    let fim = Number(txtFim.value)
-    let passo = Number(txtPasso.value)
+    let txtInicio = document.querySelector('#iinicio')
+    let txtFim = document.querySelector('#ifim')
+    let txtPasso = document.querySelector('#ipasso')
 
-    if (passo < 1) {
+    info.innerText = ''
+    if (txtInicio.value.length == 0 || txtFim.value.length == 0 || txtPasso.value.length == 0) {
+        alert('[Erro] Faltam Dados')
         contando.innerHTML = ''
-        alert('Passo inválido! Considerando passo 1')
-        passo = 1
-    } else if (inicio == 0) {
-        info.innerHTML = 'Impossível contar sem um valor de início! Escolha um número.'
-    } if (inicio > fim) {
-        contando.innerHTML = 'Contando..'
-        info.innerHTML = ''
-        for (inicio; inicio >= fim; inicio -= passo) {
-            info.innerHTML += `${inicio} 👉`
-        }
-        info.innerHTML += '🏁'
+        info.innerHTML = 'Preencha os dados para realizar a contagem.'
     } else {
-        info.innerHTML = ``
-        contando.innerHTML = 'Contando..'
-        for (inicio; inicio <= fim; inicio += passo) {
-            info.innerHTML += `${inicio} 👉`
+        contando.innerHTML = 'Contando: <br>' 
+        let inicio = Number(txtInicio.value)
+        let fim = Number(txtFim.value)
+        let passo = Number(txtPasso.value)
+        
+        if (passo <= 0) {
+            alert('Impossível conta! Considerando passo = 1')
+            passo = 1
         }
-        info.innerHTML += '🏁'
+        // Contangem crescente
+        if (inicio < fim) {
+            for (let c = inicio; c <= fim; c+= passo) {
+                contando.innerHTML += ` ${c} 👉`
+            }
+        } else {
+
+            // Contagem regressiva
+            for (let c = inicio; c >= fim; c-= passo) {
+                contando.innerHTML += ` ${c} 👉`
+            }
+        }
+        contando.innerHTML += `🏁`
     }
 }
