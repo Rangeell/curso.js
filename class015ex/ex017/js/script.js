6922e55dae60dee0ebc4f66fc8277afa1298cdd4
Rangel <breno.r.ramos1@gmail.com>
2026-05-11 14:01:19 -0300

    att ex017

--- class015ex/ex017/js/script.js
+++ class015ex/ex017/js/script.js
@@ -1,23 +1,18 @@
-let txtNumber = document.querySelector('#inumber')
-let txtTabuada = document.querySelector('#itabuada')
-
 let button = document.querySelector('input[type="button')
-
-
-
 button.addEventListener('click', gerarTabuada)
 
 function gerarTabuada() {
+    let txtNumber = document.querySelector('#inumber')
+    let txtTabuada = document.querySelector('#itabuada')
+
     txtTabuada.innerHTML = ''
-    let mult = 1
     
     if (txtNumber.value.length == '') {
         alert('Você precisa digitar um número para gerarmos a tabuada!')
     } else {
         let number = Number(txtNumber.value)
-        for (mult; mult <= 10; mult++) {
-            let res = number * mult
-            txtTabuada.innerHTML += `${number} x ${mult} = ${res}\n`
+        for (let mult = 1; mult <= 10; mult++) {
+            txtTabuada.innerHTML += `${number} x ${mult} = ${number * mult}\n`
         }
     }
 
